7156c03a19f6757268a6d08c2bcd8f2121428091
lucia <lucia.t.cardozo@gmail.com>
2019-08-28 22:17:44 -0300

    falta validar y refinar

--- 04/08/Clase_03.1/src/Clase_03.1.c
+++ 04/08/Clase_03.1/src/Clase_03.1.c
@@ -10,8 +10,55 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define CANTIDAD 5
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+int sumarDosEnteros(float numeroUno, float numeroDos,float *resultado);
+int sacarNumeroMaxMin(float numero,float*numMax,float *numMin);
+
+int main()
+{
+	float numeroA;
+	float numMax;
+	float numMin;
+	float acumSuma=0;
+	float promTotal;
+	int cantidad;
+	int i;
+	printf("Ingrese la cantidad de numero que quiere ingresar: ");
+	fflush(stdin);
+	scanf("%d",&cantidad);
+	for(i=0;i<cantidad;i++)
+	{
+		printf("Ingrese numero: ");
+		fflush(stdin);
+		scanf("%f",&numeroA);
+		sacarNumeroMaxMin(numeroA,&numMax,&numMin);
+		acumSuma = acumSuma + numeroA;
+	}
+	    printf("Suma de los numeros ingresados: %.2f",acumSuma);
+	    printf("\nNumero Minimo: %.2f",numMin);
+	    printf("\nNumero Maximo: %.2f",numMax);
+	    promTotal=acumSuma/cantidad;
+	    printf("\nPromedio de Numeros Sumados: %.2f",promTotal);
+
+	return 0;
+}
+
+int sumarDosEnteros(float numeroUno, float numeroDos,float *resultado)
+{
+	*resultado = numeroUno+numeroDos;
+	return 0;
+}
+
+int sacarNumeroMaxMin(float numero,float*numMax,float *numMin)
+{
+    if(numero>*numMax)
+    {
+        *numMax=numero;
+    }
+    if(numero<*numMin)
+    {
+        *numMin=numero;
+    }
+    return 0;
 }
--- 04/08/Clase_3.28-08/main.c
+++ 04/08/Clase_3.28-08/main.c
@@ -1,8 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int sumarDosEnteros(float numeroUno, float numeroDos,float *resultado);
+int sacarNumeroMaxMin(float numero,float*numMax,float *numMin);
+
 int main()
 {
-    printf("Hello world!\n");
+	float numeroA;
+	float numMax;
+	float numMin;
+	float acumSuma=0;
+	float promTotal;
+	int cantidad;
+	int i;
+	printf("Ingrese la cantidad de numero que quiere ingresar: ");
+	fflush(stdin);
+	scanf("%d",&cantidad);
+	for(i=0;i<cantidad;i++)
+	{
+		printf("Ingrese numero: ");
+		fflush(stdin);
+		scanf("%f",&numeroA);
+		sacarNumeroMaxMin(numeroA,&numMax,&numMin);
+		acumSuma = acumSuma + numeroA;
+	}
+	    printf("Suma de los numeros ingresados: %.2f",acumSuma);
+	    printf("\nNumero Minimo: %.2f",numMin);
+	    printf("\nNumero Maximo: %.2f",numMax);
+	    promTotal=acumSuma/cantidad;
+	    printf("\nPromedio de Numeros Sumados: %.2f",promTotal);
+
+	return 0;
+}
+
+int sumarDosEnteros(float numeroUno, float numeroDos,float *resultado)
+{
+	*resultado = numeroUno+numeroDos;
+	return 0;
+}
+
+int sacarNumeroMaxMin(float numero,float*numMax,float *numMin)
+{
+    if(numero>*numMax)
+    {
+        *numMax=numero;
+    }
+    if(numero<*numMin)
+    {
+        *numMin=numero;
+    }
     return 0;
 }
+
+
+/*if(numeroA<numMin)
+		{
+            numMin=numeroA;
+		}
+		else if(numeroA>numMax)
+		{
+            numMax=numeroA;
+		}
+		*/
